29773915feebc13b2d768d366fe84058710617fc
amorgrace <akanforte@gmail.com>
2026-03-08 19:28:31 +0100

    cors updated

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:5500"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
   })
@@ -28,7 +28,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}/api/docs`);
 });
 
 
--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -2,9 +2,14 @@ import swaggerAutogen from "swagger-autogen";
 
 const swagger = swaggerAutogen();
 
+const doc = {
+  host: "student-grading-app.pxxl.click",
+  schemes: ["https", "http"],
+};
+
 const outputFile = "./swagger-output.json";
 const endpointsFiles = ["../server.ts"];
 
-swagger(outputFile, endpointsFiles).then(() => {
+swagger(outputFile, endpointsFiles, doc).then(() => {
   console.log("Swagger docs generated");
 });
